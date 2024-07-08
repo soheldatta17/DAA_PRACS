faaf8e0de0979d84ac4604739b1f2a87b26ab547
soheldatta17 <soheldatta17@gmail.com>
2024-07-09 02:49:44 +0530

    .

---
+++ linked_list.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    struct Node *next;
+    int data;
+}*head,*current;
+void displayNode()
+{
+    struct Node *temp=head;
+    while (temp!=NULL)
+    {
+        printf("\n%d",temp->data);
+        temp=temp->next;
+    }
+    
+}
+void createNode(int n)
+{
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data=n;
+    if(head==NULL)
+    {
+        head=newnode;
+        head->next = NULL;
+        current=head;
+    }
+    else
+    {
+        current->next=newnode;
+        current->next->next=NULL;
+        current=newnode;
+    }
+}
+void main()
+{
+    int n,i;
+    printf("Enter the number of Nodes: ");
+    scanf("%d",&n);
+    printf("\n");
+    for(i=0;i<n;i++)
+    {
+        createNode(i);
+    }
+    displayNode(i);
+}
